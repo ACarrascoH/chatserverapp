2e7f17b335c4bffe22f69d42a3182f7279f41dff
Angelo Carrasco <angelo1rayo@gmail.com>
2021-04-06 17:05:59 -0400

    Agrega Servidor Socket

--- ChatSocketApp/ChatSocketApp/Program.cs
+++ ChatSocketApp/ChatSocketApp/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿
+using ChatSocketApp.Comunicacion;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,40 @@ namespace ChatSocketApp
     {
         static void Main(string[] args)
         {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            ServerSocket serverSocket = new ServerSocket(puerto);
+            if (serverSocket.Iniciar())
+            {
+                //puedo esperar un cliente
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Esperando cliente...");
+                    if (serverSocket.ObtenerCliente())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Cliente recibido!");
+                        Console.WriteLine("S:Hola cliente!");
+                        serverSocket.Escribir("Hola Cliente");
+                        Console.WriteLine("Escriba un mensaje para el cliente:");
+                        string mensaje = Console.ReadLine().Trim();
+                        Console.WriteLine("S:{0}", mensaje);
+                        serverSocket.Escribir(mensaje);
+
+                        //mensaje recibido del cliente
+                        string respuesta = serverSocket.Leer();
+                        Console.WriteLine("C:{0}", respuesta);
+                        serverSocket.CerrarConexion();
+                    }
+                }
+            }
+            else
+            {
+                //por permisos no puedo levantar la comunicacion en el puerto
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No se puede levantar el servidor, puerto ocupado o sin privilegios");
+                Console.ReadKey();
+            }
         }
     }
 }
